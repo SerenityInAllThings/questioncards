f8b72baecc6b699ca23d92dc12b87a2369490f90
Peterson v <peterson.victor@mercadolivre.com>
2020-08-29 20:26:27 -0300

    First commit

---
+++ src/main/kotlin/petersonv/com/questioncards/QuestioncardsApplication.kt
@@ -0,0 +1,11 @@
+package petersonv.com.questioncards
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuestioncardsApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuestioncardsApplication>(*args)
+}
---
+++ src/test/kotlin/petersonv/com/questioncards/QuestioncardsApplicationTests.kt
@@ -0,0 +1,13 @@
+package petersonv.com.questioncards
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuestioncardsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
